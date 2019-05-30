ce567086ba0a98316d80016c6b9e1593c4f88158
jjacob <Joseph.Jacob@jpl.nasa.gov>
2019-05-29 17:24:57 -0700

    topsStack PGE initial commit

---
+++ get_bbox.py
@@ -0,0 +1,32 @@
+import json
+from math import floor, ceil
+
+
+def load_context():
+    '''loads the context file into a dict'''
+    try:
+        context_file = '_context.json'
+        with open(context_file, 'r') as fin:
+            context = json.load(fin)
+        return context
+    except:
+        raise Exception('unable to parse _context.json from work directory')
+
+def main():
+    ctx = load_context()
+    min_lat = ctx['min_lat']
+    max_lat = ctx['max_lat']
+    min_lon = ctx['min_lon']
+    max_lon = ctx['max_lon']
+    min_lat_lo = floor(min_lat)
+    max_lat_hi = ceil(max_lat)
+    min_lon_lo = floor(min_lon)
+    max_lon_hi = ceil(max_lon)
+    out = ' '.join(str(e) for e in (min_lat, max_lat, min_lon, max_lon,
+                                    min_lat_lo, max_lat_hi,
+                                    min_lon_lo, max_lon_hi))
+    print(out)
+    
+
+if __name__ == '__main__':
+    main()
---
+++ run_stack.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+set -e 
+
+# Get user's bounding box coordinates and outer box with integer coordinates
+TOKENS=$(python get_bbox.py _context.json)
+IFS=" "
+read MINLAT MAXLAT MINLON MAXLON MINLAT_LO MAXLAT_HI MINLON_LO MAXLON_HI <<< $TOKENS
+
+# Prep SLCs
+#mkdir zip
+#mv S1*/*.zip zip
+
+# Get DEM
+echo dem.py -a stitch -b $MINLAT_LO $MAXLAT_HI $MINLON_LO $MAXLON_HI -r -s 1 -c -f
+
+# Fix DEM
+if [[ $MINLAT_LO < 0 ]]; then
+    MINLAT_LO=$(( -$MINLAT_LO ))
+    NS_LO=S
+else
+    NS_LO=N
+fi
+if [[ $MAXLAT_HI < 0 ]]; then
+    MAXLAT_HI=$(( -$MAXLAT_HI ))
+    NS_HI=S
+else
+    NS_HI=N
+fi
+if [[ $MINLON_LO < 0 ]]; then
+    MINLON_LO=$(( -$MINLON_LO ))
+    EW_LO=W
+else
+    EW_LO=E
+fi
+if [[ $MAXLON_HI < 0 ]]; then
+    MAXLON_HI=$(( -$MAXLON_HI ))
+    EW_HI=W
+else
+    EW_LO=E
+fi
+WGS84=demLat_${NS_LO}${MINLAT_LO}_${NS_HI}${MAXLAT_HI}_Lon_${EW_LO}${MINLON_LO}_${EW_HI}${MAXLON_HI}.dem.wgs84
+if [[ -z $WGS84 ]]; then
+    echo fixImageXml.py -f -i $WGS84
+else
+    echo $WGS84 does not exist!
+    exit 1
+fi
+# Create stack processor run scripts
+echo stackSentinel.py -s zip/ -d $WGS84 -a AuxDir/ -o Orbits -b \'$MINLAT $MAXLAT $MINLON $MAXLON\' -W slc
+
+# Process stack processor run scripts in order
+# Do the following in a loop?  
+exit 0
+source ./run_files/run_1_unpack_slc_topo_master
+source ./run_files/run_2_average_baseline
+source ./run_files/run_3_extract_burst_overlaps
+source ./run_files/run_4_overlap_geo2rdr_resample
+source ./run_files/run_5_pairs_misreg
+source ./run_files/run_6_timeseries_misreg
+source ./run_files/run_7_geo2rdr_resample
+source ./run_files/run_8_extract_stack_valid_region
+source ./run_files/run_9_merge
+source ./run_files/run_10_grid_baseline
