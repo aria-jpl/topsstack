6dac921b73f2a52ac92ab24ce60bbccb1564ff82
torresal <alexander.p.torres@jpl.nasa.gov>
2019-12-06 07:25:33 +0000

    updated search for wgs84 file

--- run_stack.sh
+++ run_stack.sh
@@ -22,42 +22,56 @@ mv S1*/*.zip zip
 
 # Get DEM
 echo dem.py -a stitch -b $MINLAT_LO $MAXLAT_HI $MINLON_LO $MAXLON_HI -r -s 1 -c -f
-dem.py -a stitch -b $MINLAT_LO $MAXLAT_HI $MINLON_LO $MAXLON_HI -r -s 1 -c -f
+dem.py -a stitch -b $MINLAT_LO $MAXLAT_HI $MINLON_LO $MAXLON_HI -r -s 1 -c -f >&1 | tee dem.txt
+
+if [[ -f dem.txt ]]; 
+then
+    WGS84=`awk '/wgs84/ {print $NF;exit}' dem.txt`
+    if [[ -f "${WGS84}" ]]; 
+    then
+	echo "Found wgs84 file: ${WGS84}"
+        echo "fixImageXml.py -f -i ${WGS84}"
+        fixImageXml.py -f -i ${WGS84}
+    else
+        echo "NO WGS84 FILE FOUND" 
+        exit 1
+    fi
+fi
 
 
-# Fix DEM
-if [[ $MINLAT_LO < 0 ]]; then
-    MINLAT_LO=$(( -$MINLAT_LO ))
-    NS_LO=S
-else
-    NS_LO=N
-fi
-if [[ $MAXLAT_HI < 0 ]]; then
-    MAXLAT_HI=$(( -$MAXLAT_HI ))
-    NS_HI=S
-else
-    NS_HI=N
-fi
-if [[ $MINLON_LO < 0 ]]; then
-    MINLON_LO=$(( -$MINLON_LO ))
-    EW_LO=W
-else
-    EW_LO=E
-fi
-if [[ $MAXLON_HI < 0 ]]; then
-    MAXLON_HI=$(( -$MAXLON_HI ))
-    EW_HI=W
-else
-    EW_LO=E
-fi
-WGS84=demLat_${NS_LO}${MINLAT_LO}_${NS_HI}${MAXLAT_HI}_Lon_${EW_LO}${MINLON_LO}_${EW_HI}${MAXLON_HI}.dem.wgs84
-if [[ -f $WGS84 ]]; then
-    echo fixImageXml.py -f -i $WGS84
-    fixImageXml.py -f -i $WGS84
-else
-    echo $WGS84 does not exist!
-    exit 1
-fi
+## Fix DEM
+#if [[ $MINLAT_LO < 0 ]]; then
+#    MINLAT_LO=$(( -$MINLAT_LO ))
+#    NS_LO=S
+#else
+#    NS_LO=N
+#fi
+#if [[ $MAXLAT_HI < 0 ]]; then
+#    MAXLAT_HI=$(( -$MAXLAT_HI ))
+#    NS_HI=S
+#else
+#    NS_HI=N
+#fi
+#if [[ $MINLON_LO < 0 ]]; then
+#    MINLON_LO=$(( -$MINLON_LO ))
+#    EW_LO=W
+#else
+#    EW_LO=E
+#fi
+#if [[ $MAXLON_HI < 0 ]]; then
+#    MAXLON_HI=$(( -$MAXLON_HI ))
+#    EW_HI=W
+#else
+#    EW_LO=E
+#fi
+#WGS84=demLat_${NS_LO}${MINLAT_LO}_${NS_HI}${MAXLAT_HI}_Lon_${EW_LO}${MINLON_LO}_${EW_HI}${MAXLON_HI}.dem.wgs84
+#if [[ -f $WGS84 ]]; then
+#    echo fixImageXml.py -f -i $WGS84
+#    fixImageXml.py -f -i $WGS84
+#else
+#    echo $WGS84 does not exist!
+#    exit 1
+#fi
 
 
 # Getting MASTER_DATE environment variable
@@ -66,7 +80,7 @@ export MASTER_DATE=$(python ${PGE_BASE}/get_master_date.py)
 # Create stack processor run scripts (after checking for MASTER DATE)
 if [[ "$MASTER_DATE" ]]; then
 	echo "MASTER_DATE exists: ${MASTER_DATE}"
-	echo stackSentinel.py -s zip/ -d $WGS84 -a AuxDir/ -m $MASTER_DATE -o Orbits -b "$MINLAT $MAXLAT $MINLON $MAXLON" -W slc
+	echo stackSentinel.py -s zip/ -d ${WGS84} -a AuxDir/ -m ${MASTER_DATE} -o Orbits -b "${MINLAT} ${MAXLAT} ${MINLON} ${MAXLON}" -W slc
 	stackSentinel.py -s zip/ -d $WGS84 -a AuxDir/ -m $MASTER_DATE -o Orbits -b "$MINLAT $MAXLAT $MINLON $MAXLON" -W slc
 else
 	echo "MASTER_DATE DOES NOT EXIST"
@@ -183,3 +197,4 @@ echo "@ Step 10:  $runtime10"
 
 # Publishing dataset after stack processor completes
 python /home/ops/verdi/ops/topsstack/create_dataset.py
+'
