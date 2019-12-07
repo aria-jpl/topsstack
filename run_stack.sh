8720de8da95a907f2b2809a2029e06f02c764564
torresal <alexander.p.torres@jpl.nasa.gov>
2019-12-06 18:19:42 -0800

    revised run_stack.sh

--- run_stack.sh
+++ run_stack.sh
@@ -24,6 +24,7 @@ mv S1*/*.zip zip
 echo dem.py -a stitch -b $MINLAT_LO $MAXLAT_HI $MINLON_LO $MAXLON_HI -r -s 1 -c -f
 dem.py -a stitch -b $MINLAT_LO $MAXLAT_HI $MINLON_LO $MAXLON_HI -r -s 1 -c -f >&1 | tee dem.txt
 
+# Find wgs84 file
 if [[ -f dem.txt ]]; 
 then
     WGS84=`awk '/wgs84/ {print $NF;exit}' dem.txt`
@@ -37,42 +38,6 @@ then
         exit 1
     fi
 fi
-
-
-## Fix DEM
-#if [[ $MINLAT_LO < 0 ]]; then
-#    MINLAT_LO=$(( -$MINLAT_LO ))
-#    NS_LO=S
-#else
-#    NS_LO=N
-#fi
-#if [[ $MAXLAT_HI < 0 ]]; then
-#    MAXLAT_HI=$(( -$MAXLAT_HI ))
-#    NS_HI=S
-#else
-#    NS_HI=N
-#fi
-#if [[ $MINLON_LO < 0 ]]; then
-#    MINLON_LO=$(( -$MINLON_LO ))
-#    EW_LO=W
-#else
-#    EW_LO=E
-#fi
-#if [[ $MAXLON_HI < 0 ]]; then
-#    MAXLON_HI=$(( -$MAXLON_HI ))
-#    EW_HI=W
-#else
-#    EW_LO=E
-#fi
-#WGS84=demLat_${NS_LO}${MINLAT_LO}_${NS_HI}${MAXLAT_HI}_Lon_${EW_LO}${MINLON_LO}_${EW_HI}${MAXLON_HI}.dem.wgs84
-#if [[ -f $WGS84 ]]; then
-#    echo fixImageXml.py -f -i $WGS84
-#    fixImageXml.py -f -i $WGS84
-#else
-#    echo $WGS84 does not exist!
-#    exit 1
-#fi
-
 
 # Getting MASTER_DATE environment variable
 export MASTER_DATE=$(python ${PGE_BASE}/get_master_date.py)
